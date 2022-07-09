fee11da4ef3c14606afb28d0ebdb3b193a59f3f0
임병욱 <imbyeong-ug@imbyeong-ug-ui-MacBookPro.local>
2022-07-09 15:29:39 +0900

    useMemo

--- diaryagain2/src/App.js
+++ diaryagain2/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
@@ -24,6 +24,17 @@ const App = () => {
     });
     setData(initData);
   };
+
+  const getDiaryAnalsis = useMemo(() => {
+    console.log("일기 분석 시작");
+
+    const goodCount = data.filter((item) => item.emotion > 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalsis;
 
   useEffect(() => {
     getData();
@@ -58,6 +69,10 @@ const App = () => {
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div>전체일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
