d909b760ede56ea08598e23ba7d55be92ae3344b
임병욱 <imbyeong-ug@imbyeong-ug-ui-MacBookPro.local>
2022-07-18 10:37:55 +0900

    MyHeader

--- diaryclone/src/App.js
+++ diaryclone/src/App.js
@@ -5,11 +5,18 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import New from "./pages/New";
+import MyHeader from "./compontents/MyHeader";
+import MyButton from "./compontents/MyButton";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={<MyButton text={"왼쪽 버튼"} />}
+          rightChild={<MyButton text={"오른쪽 버튼"} />}
+        />
         <h2>App.js</h2>
         <Routes>
           <Route path="/" element={<Home />} />
